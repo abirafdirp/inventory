96f868d24b9ac9a30b59a68969d0963a44dc3f07
abirafdirp <abirafdiraditya@gmail.com>
2015-07-06 21:23:49 +0700

    v0.1.0 items model feature complete

--- inventory/items/admin.py
+++ inventory/items/admin.py
@@ -14,15 +14,30 @@ class BaseItemAdmin(admin.ModelAdmin):
                     'expires_in', 'created','modified')
 
     # search fields help text is in inventory/templates/admin/search_form.html
-    search_fields = ['name', 'category__name', 'brand__name','expires_in']
+    search_fields = ['name', 'category__name', 'brand__name','expires_in','sku','expires_in',
+                     'description', 'product_id_prefix__name']
     list_filter =  ('expireable', 'brand', 'category')
+
+    readonly_fields = ('expireable',)
 
     # get_categories function renamed to categories
     def categories(self, obj):
         return ", ".join([p.name for p in obj.category.all()])
 
 class ItemAdmin(admin.ModelAdmin):
-    pass
+    fields = ('base_item', 'product_id', 'expired',
+            'location',
+    )
+    readonly_fields = ('product_id', 'expired')
+    list_display = ('Name', 'product_id', 'location', 'expiration_date', 'expired')
+    search_fields = ['base_item__name', 'product_id', 'location__name']
+    def get_sku(self, instance):
+        return instance.base_item.sku
+    # get name function renamed to Name
+    def Name(self, instance):
+        return  instance.base_item.name
+
+
 class ProductIdPrefixAdmin(admin.ModelAdmin):
     pass
 
@@ -31,4 +46,6 @@ admin.site.register(Category, CategoryAdmin)
 admin.site.register(BaseItem, BaseItemAdmin)
 admin.site.register(Item, ItemAdmin)
 admin.site.register(ProductIdPrefix, ProductIdPrefixAdmin)
+
+
 
--- inventory/items/models.py
+++ inventory/items/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import datetime
+from django.utils import timezone
+import random, string
 # Create your models here.
 
 class TimeStampedModel(models.Model):
@@ -42,7 +44,8 @@ class Category(NameModel, TimeStampedModel):
     class Meta:
         verbose_name_plural = "Categories"
 
-class ProductIdPrefix(NameModel):
+class ProductIdPrefix(models.Model):
+    name = models.CharField(max_length=7, help_text='Max length 7 characters')
 
     def __str__(self):
         return self.name
@@ -52,15 +55,24 @@ class ProductIdPrefix(NameModel):
 
 class BaseItem(NameModel, TimeStampedModel):
     sku = models.CharField(max_length=20, verbose_name='SKU')
+    product_id_prefix = models.ForeignKey(ProductIdPrefix, null=True, blank=True,
+                                          verbose_name='Product ID prefix')
     brand = models.ForeignKey(Brand, related_name='brand_of')
     category = models.ManyToManyField(Category, related_name='category_of')
     description = models.CharField(max_length=50, blank=True)
     image = models.ImageField(upload_to='items', blank=True)
     expireable = models.BooleanField(default=False)
-    expires_in = models.DateField(null=True, blank=True)
+    expires_in = models.IntegerField(null=True, blank=True, default=0, verbose_name='Expires in (days)',
+                                  help_text='This is NOT expiration date, but how long until '+
+                                  'this item will be expired in days. Leave blank if the item'+
+                                  ' is not expireable')
 
     def save(self, *args, **kwargs):
         self.modified = datetime.datetime.today()
+        if self.expires_in == 0:
+            self.expireable = False
+        else:
+            self.expireable = True
         super(BaseItem, self).save(*args, **kwargs)
 
     def __str__(self):
@@ -70,23 +82,38 @@ class Item(TimeStampedModel):
     base_item = models.ForeignKey(BaseItem, related_name='base_item_of')
 
     # the product id will be randomly generated from its prefix
-    product_id_prefix = models.ForeignKey(ProductIdPrefix, null=True)
-    product_id = models.CharField(max_length=15,
-                                  help_text='Randomly generated from Product ID Prefix'+
-                                            ' if left blank. max length 15 chars')
+    # because the product ID is given by the supplier
+    product_id = models.CharField(max_length=15, blank=True,
+                                  help_text='Product ID will be generated randomly to '+
+                                'simulate shipment from supplier'
+                                )
+    expiration_date = models.DateField(editable=False, blank=True)
     expired = models.BooleanField(default=False)
 
     # prevent circular import
     location = models.ForeignKey('transaction.Location', related_name='location_of')
 
     def save(self, *args, **kwargs):
-        self.modified = datetime.datetime.today()
-
-        if not self.product_id_prefix:
-
+        self.modified = timezone.now()
+        expires_in = self.base_item.expires_in
+        if expires_in == 0:
+            self.expiration_date = datetime.datetime(2099, 12, 12)
+        else:
+            self.expiration_date = timezone.now() + datetime.timedelta(days=expires_in)
+        # randomized product id, 7 chars from the max length of SKU
+        # and 8 chars is randomized. Max len of product id is 15 chars
+        # if there is already one, then it will continue to be randomized
+        while True:
+            product_ID = self.base_item.product_id_prefix.name + randomword(8)
+            try:
+                Item.objects.get(product_id = product_ID)
+            except:
+                self.product_id = product_ID
+                break
         super(Item, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.base_item.name
 
-
+def randomword(length):
+    return ''.join(random.choice(string.lowercase+string.digits) for i in range(length))
