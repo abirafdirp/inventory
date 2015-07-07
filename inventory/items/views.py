4215468e26ef7d8a7736ced937a48bffc7992f34
abirafdirp <abirafdiraditya@gmail.com>
2015-07-07 22:13:02 +0700

    add more APIs, save point before refactoring to ViewSet

--- inventory/items/views.py
+++ inventory/items/views.py
@@ -1,13 +1,82 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import permissions
 from .models import Item
 from .serializers import ItemSerializer
+from .serializers import BrandSerializer
+from .serializers import CategorySerializer
+from .serializers import BaseItemSerializer
+from .permissions import IsOwnerOrReadOnly
 # Create your views here.
+
+class BrandList(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = BrandSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class BrandDetail(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = BrandSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class CategoryList(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class CategoryDetail(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class BaseItemList(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = BaseItemSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class BaseItemDetail(generics.RetrieveAPIView):
+    queryset = Item.objects.all()
+    serializer_class = BaseItemSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class ItemList(generics.ListCreateAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class ItemDetail(generics.RetrieveAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
