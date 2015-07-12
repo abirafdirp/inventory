ba15270381c784a78fa038c996ba34ec175c31ed
Abirafdi Raditya Putra <abirafdiraditya@gmail.com>
2015-07-12 20:24:32 +0700

    condense function in populate_db

--- inventory/populate_db.py
+++ inventory/populate_db.py
@@ -20,25 +20,53 @@ def populate():
 
     # get user for auth, username is from command line argument
     username = str(sys.argv[1])
-    password = str(sys.argv[2])
     try:
         owner = User.objects.get(username=username)
-        print owner.password
     except:
         print 'invalid username or password'
         return
-    # add_category()
 
-def add_page(cat, title, url, views=0):
-    p = BaseItem.objects.get_or_create(category=cat, title=title)[0]
-    p.url=url
-    p.views=views
-    p.save()
-    return p
+    smartphone = add_category(owner=owner, name='Smartphone')
+
+    samsung = add_brand(owner=owner, name='Samsung')
+
+    glxy6prefix = add_productidprefix(owner=owner, name='SMGG6-')
+
+    glxy6base = add_baseitem(owner=owner, name='Galaxy S6', sku='SMSNGGLXY6',
+                             brand=samsung, product_id_prefix=glxy6prefix,
+                             category=smartphone, expires_in=0)
+
+# def additem()
+
+def add_baseitem(owner, name, sku, brand, category, product_id_prefix,
+                 expires_in, description='', image=''):
+    b = BaseItem.objects.get_or_create\
+        (owner=owner, name=name, sku=sku, brand=brand, category=category,
+         product_id_prefix=product_id_prefix, description=description,
+         image=image, expires_in=expires_in)
+    display(name, 'base item')
+    return b
+
+def add_productidprefix(owner, name):
+    b = ProductIdPrefix.objects.get_or_create(owner=owner, name=name)[0]
+    b.save()
+    display(name, 'product ID prefix')
+    return b
+
+def add_brand(owner, name):
+    b = Brand.objects.get_or_create(owner=owner, name=name)[0]
+    b.save()
+    display(name, 'brand')
+    return b
 
 def add_category(owner, name):
     c = Category.objects.get_or_create(owner=owner, name=name)[0]
+    b.save()
+    display(name, 'category')
     return c
+
+def display(name, model):
+    print 'Added %s in %s' %(name, model)
 
 # Start execution here!
 if __name__ == '__main__':
