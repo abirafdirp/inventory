b5e0a8dd9c9de4e18a42f56d119c5e4e3f815db9
abirafdirp <abirafdiraditya@gmail.com>
2015-07-21 18:31:46 +0700

    sync

--- inventory/inventory/static/js/app.js
+++ inventory/inventory/static/js/app.js
@@ -1,18 +1,39 @@
-var app = angular.module('inventory', ['ngRoute', 'ui.bootstrap']);
+var api = angular.module('inventory.api', ['ngResource']);
+
+api.factory('BaseItem', [
+  '$resource', function($resource) {
+    return $resource('/apiv1/baseitems/:id', {
+      id: '@id'
+    });
+  }
+]);
+
+api.factory('Post', [
+  '$resource', function($resource) {
+    return $resource('/api/posts/:id', {
+      id: '@id'
+    });
+  }
+]);
+
+api.factory('Photo', [
+  '$resource', function($resource) {
+    return $resource('/api/photos/:id', {
+      id: '@id'
+    });
+  }
+]);
+
+
+var app = angular.module('inventory', ['ngRoute', 'ui.bootstrap', 'inventory.api']);
+
+app.controller('MainController', [
+  '$scope', 'BaseItem', function($scope, BaseItem) {
+    return $scope.baseitems= BaseItem.get();
+  }
+]);
 
 app.config(function($interpolateProvider) {
     $interpolateProvider.startSymbol('{$');
     $interpolateProvider.endSymbol('$}');
 });
-
-app.controller('MainController', [
-  '$scope', '$http', function($scope, $http) {
-    $scope.baseitem = [];
-    return $http.get('/apiv1/baseitems/?format=json').then(function(result) {
-      return angular.forEach(result.data, function(item) {
-        return $scope.baseitem.push(item);
-      });
-    });
-  }
-]);
-
