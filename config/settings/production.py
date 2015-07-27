a161b4e804355e9d3fcb73a9ae5051c2356e6b4f
abirafdi raditya putra <abirafdirp@yahoo.com>
2015-07-27 08:56:19 +0700

    change database url back again

--- config/settings/production.py
+++ config/settings/production.py
@@ -85,7 +85,7 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [
 # DATABASE CONFIGURATION
 # ------------------------------------------------------------------------------
 # Raises ImproperlyConfigured exception if DATABASE_URL not in os.environ
-DATABASES['default'] = env.db("OPENSHIFT_inventory_DB_URL")
+DATABASES['default'] = env.db("DATABASE_URL")
 
 # CACHING
 # ------------------------------------------------------------------------------
